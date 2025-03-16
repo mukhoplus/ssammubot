d1ae6b1a84f86143c06dac09bb783b373535c780
Mukho <mukho@kakao.com>
2025-03-16 15:32:53 +0900

    first commit

---
+++ src/main/kotlin/com/mukho/ssammubot/쌈무봇Application.kt
@@ -0,0 +1,11 @@
+package com.mukho.ssammubot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class 쌈무봇Application
+
+fun main(args: Array<String>) {
+	runApplication<쌈무봇Application>(*args)
+}
---
+++ src/test/kotlin/com/mukho/ssammubot/쌈무봇ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mukho.ssammubot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class 쌈무봇ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
