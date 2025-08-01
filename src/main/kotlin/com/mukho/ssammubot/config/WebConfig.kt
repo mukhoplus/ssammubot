de00c81c6f6ec778e641c510a7b809b02ca44133
Mukho <mukho@kakao.com>
2025-08-02 01:38:35 +0900

    Mukho: Public 배포용 URL 추가

--- src/main/kotlin/com/mukho/ssammubot/config/WebConfig.kt
+++ src/main/kotlin/com/mukho/ssammubot/config/WebConfig.kt
@@ -13,7 +13,7 @@ class WebConfig(
     
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-            .allowedOriginPatterns("https://smweb.mukho.r-e.kr", "http://localhost:5173", "http://localhost:8080")
+            .allowedOriginPatterns("https://smweb.mukho.r-e.kr", "https://ssammubot.vercel.app/", "http://localhost:5173", "http://localhost:8080")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true)
