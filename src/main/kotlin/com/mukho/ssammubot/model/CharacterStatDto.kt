4287986028109e91b8e6cff2f245ca636db92f30
Mukho <mukho@kakao.com>
2025-09-15 01:54:07 +0900

    Fix: 로그를 통한 JSON 관련 에러 해결

--- src/main/kotlin/com/mukho/ssammubot/model/CharacterStatDto.kt
+++ src/main/kotlin/com/mukho/ssammubot/model/CharacterStatDto.kt
@@ -1,8 +1,13 @@
 package com.mukho.ssammubot.model
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class CharacterStatDto(
     val date: String?,
+    @JsonProperty("character_class")
     val character_class: String,
+    @JsonProperty("final_stat")
     val final_stat: List<FinalStatDto>,
+    @JsonProperty("remain_ap")
     val remain_ap: Number
 )
